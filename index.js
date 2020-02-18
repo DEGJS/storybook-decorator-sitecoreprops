dea433fe2c20df9976ce7b7f6d9b4d5bce196e46
Aaron Ladage <aladage@degdigital.com>
2020-02-18 15:40:22 -0600

    field prop check

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import React, { cloneElement, Children } from 'react';
 import { makeDecorator } from '@storybook/addons';
 
 const createFieldsObject = fields => {
-  if (!fields) {
-    return {};
-  }
   return Object.keys(fields).reduce((acc, key) => {
     const value = fields[key];
     const isStringValue = typeof value === 'string';
@@ -13,21 +10,25 @@ const createFieldsObject = fields => {
   }, {});
 };
 
-const StoryContainer = ({ storyProps, children }) => {
-  const childComponents = Children.map(children, child => {
+const StoryContainer = ({ storyProps, children }) =>
+  Children.map(children, child => {
+    if (!storyProps.fields) {
+      return child;
+    }
     return cloneElement(child, {
       ...storyProps,
       fields: createFieldsObject(storyProps.fields)
     });
   });
-  return childComponents;
-};
 
 export const withSitecoreProps = makeDecorator({
   name: 'withSitecoreProps',
   allowDeprecatedUsage: false,
   wrapper: (getStory, context) => {
     const story = getStory(context);
+    if (!story.props.fields) {
+      return story;
+    }
     return (
       <StoryContainer storyProps={story.props}>
         {story}
